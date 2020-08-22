22d1bd049b26ef7caaa24434ae01b08f326066c7
Richard Carter <richard@richardcarter.org>
2020-08-22 02:02:31 -0500

    this might be everything idk

--- compressors/compressors.lua
+++ compressors/compressors.lua
@@ -45,13 +45,13 @@ function findDeliveryTransposerForItem(itemName)
         -- which has known slots
         for slot=1,9 do
             local stack = proxy.getStackInSlot(D_MEINTERFACE_SIDE, slot)
-            if stack["name"] == itemName then
-                local ret = {}
-                ret["addr"] = addr
-                ret["proxy"] = proxy
-                ret["slot"] = slot
-                cacheItemToDeliveryTransposer[itemName] = ret
-                return ret
+            if stack ~= nil and stack.name == itemName then
+                local delivery = {}
+                delivery.addr = addr
+                delivery.proxy = proxy
+                delivery.slot = slot
+                cacheItemToDeliveryTransposer[itemName] = delivery
+                return delivery
             end
         end
     end
@@ -64,7 +64,7 @@ function firstEmptySlot(transposer, side)
     local stacks = transposer.getAllStacks(side)
     local slot = 0
     while true do
-        local stack = inputStacks()
+        local stack = stacks()
         slot = slot + 1
         if stack == nil then
             return nil
@@ -75,33 +75,97 @@ function firstEmptySlot(transposer, side)
     end
 end
 
--- get a stack from the recipe transposer input crate
-local inputStacks = recipeTransposer.getAllStacks(R_INPUT_CRATE)
-local slot = 0
-while true do
-    local stack = inputStacks()
-    slot = slot + 1
-    if stack == nil then
-        break
+local jobQueue = {}
+
+function pollInputCrate()
+    local outputSlot = firstEmptySlot(recipeTransposer, R_OUTPUT_CRATE)
+    if outputSlot == nil then
+        print("No empty slots in output crate")
+        return
     end
-    if next(stack) ~= nil then
-        local itemName = stack["name"]
-        local delivery = findDeliveryTransposerForItem(itemName)
-        assert(delivery ~= nil, "Could not find delivery transposer for " .. itemName)
 
-        -- move the one item to the output crate
-        local outputSlot = firstEmptySlot(recipeTransposer, R_OUTPUT_CRATE)
-        assert(outputSlot ~= nil, "No empty slots in output crate")
-        recipeTransposer.transferItem(R_INPUT_CRATE, R_OUTPUT_CRATE, 1, slot, outputSlot)
+    -- get a stack from the recipe transposer input crate
+    local inputStacks = recipeTransposer.getAllStacks(R_INPUT_CRATE)
+    local slot = 0
+    while true do
+        local stack = inputStacks()
+        slot = slot + 1
+        if stack == nil then
+            -- no input
+            break
+        end
+        if next(stack) ~= nil then
+            local itemName = stack.name
+            -- (if there is already an ongoing job for this item, leave it in the input chest)
+            if jobQueue[itemName] == nil then
+                local delivery = findDeliveryTransposerForItem(itemName)
+                if delivery == nil then
+                    print("No delivery transposer for " .. itemName)
+                else
+                    -- move the one item to the output crate
+                    recipeTransposer.transferItem(R_INPUT_CRATE, R_OUTPUT_CRATE, 1, slot, outputSlot)
+    
+                    -- queue up 9999 more items
+                    print("Queueing " .. itemName)
 
-        -- make the delivery transposer move 9999 of the item
-        print("need to move 9999 more " .. itemName)
-        break
+                    local queueItem = {}
+                    queueItem.remaining = 9999
+                    queueItem.delivery = delivery
+                    jobQueue[itemName] = queueItem
+    
+                    -- If we wanted to pull more items in this go then we would need to find
+                    -- a new output slot for each of them. Let's just break.
+                    break
+                end
+            end
+        end
+    end
+end
+
+function processJobQueue()
+    for itemName, queueItem in pairs(jobQueue) do
+        local remaining = queueItem.remaining
+        local delivery = queueItem.delivery
+
+        local stack = delivery.proxy.getStackInSlot(D_MEINTERFACE_SIDE, delivery.slot)
+        if stack ~= nil and stack.name ~= itemName then
+            print("ME interface " .. delivery.addr .. " expected " .. itemName .. " slot " .. delivery.slot .. " but was " .. stack.name)
+        elseif stack ~= nil then
+            local outputSlot = firstEmptySlot(delivery.proxy, D_CRATE_SIDE)
+            if outputSlot == nil then
+                print("Output full: " .. delivery.addr)
+            else
+                local amount = stack.size
+                if amount > remaining then
+                    amount = remaining
+                end
+                amount = delivery.proxy.transferItem(D_MEINTERFACE_SIDE, D_CRATE_SIDE, amount, delivery.slot, outputSlot)
+                queueItem.remaining = queueItem.remaining - amount
+                print("Moved " .. amount .. " " .. itemName .. ", " .. queueItem.remaining .. " remaining")
+                if queueItem.remaining <= 0 then
+                    jobQueue[itemName] = nil
+                end
+            end
+        else
+            print("Waiting for " .. itemName .. " to restock")
+        end
+    end
+end
+
+while true do
+    pollInputCrate()
+    processJobQueue()
+    if next(jobQueue) == nil then
+        print("No jobs")
+        os.sleep(10)
+    else
+        os.sleep(1)
     end
 end
 
 
-local tmpfile = filesystem.open("/tmp/output.txt", "w")
-tmpfile:write(serialization.serialize(recipeTransposer, true))
-tmpfile:close()
+
+-- local tmpfile = filesystem.open("/tmp/output.txt", "w")
+-- tmpfile:write(serialization.serialize(recipeTransposer, true))
+-- tmpfile:close()
 
