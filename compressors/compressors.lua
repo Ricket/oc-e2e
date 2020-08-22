e4111267973380baa8290c1f0e0d351e5670d4fc
Richard Carter <richard@richardcarter.org>
2020-08-21 21:21:02 -0500

    compressors: load file and open proxies

--- compressors/compressors.lua
+++ compressors/compressors.lua
@@ -1,10 +1,32 @@
 local component = require("component")
+local filesystem = require("filesystem")
 local serialization = require("serialization")
 
-local transposers = {}
+-- load the address of the recipe transposer
+local file = filesystem.open("/etc/compressors.cfg", "r")
+local recipeTransposerAddress = file:read(100)
+file:close()
+
+-- open the transposer proxies
+local recipeTransposer
+local deliveryTransposers = {}
+local deliveryTransposersCount = 0
 for address, name in component.list("transposer", false) do
-    print(address)
-    table.insert(transposers, component.proxy(address))
+    local proxy = component.proxy(address)
+    if address == recipeTransposerAddress then
+        recipeTransposer = proxy
+    else
+        deliveryTransposers[address] = proxy
+        deliveryTransposersCount = deliveryTransposersCount + 1
+    end
 end
 
-print(serialization.serialize(transposers, true))
+assert(recipeTransposer ~= nil, "Could not find the recipe transposer " .. recipeTransposerAddress)
+assert(deliveryTransposersCount > 0, "Found no delivery transposers")
+
+-- 
+
+local tmpfile = filesystem.open("/tmp/output.txt", "w")
+tmpfile:write(serialization.serialize(recipeTransposer, true))
+tmpfile:close()
+
