7224953c15d952440e2c7b046371540416b9882c
Richard Carter <richard@richardcarter.org>
2020-08-21 22:54:19 -0500

    add code to get a stack

--- compressors/compressors.lua
+++ compressors/compressors.lua
@@ -1,6 +1,14 @@
 local component = require("component")
 local filesystem = require("filesystem")
 local serialization = require("serialization")
+local sides = require("sides")
+
+-- config
+
+local R_INPUT_CRATE = sides.east
+local R_OUTPUT_CRATE = sides.up
+local D_MEINTERFACE_SIDE = sides.north
+local D_CRATE_SIDE = sides.south
 
 -- load the address of the recipe transposer
 local file = filesystem.open("/etc/compressors.cfg", "r")
@@ -24,7 +32,16 @@ end
 assert(recipeTransposer ~= nil, "Could not find the recipe transposer " .. recipeTransposerAddress)
 assert(deliveryTransposersCount > 0, "Found no delivery transposers")
 
--- 
+-- get a stack from the recipe transposer input crate
+local inputStacks = recipeTransposer.getAllStacks(R_INPUT_CRATE)
+if inputStacks.count() > 0 then
+    local stack = inputStacks()
+    print("found stack:")
+    print(serialization.serialize(stack, true))
+else
+    print("no stack")
+end
+
 
 local tmpfile = filesystem.open("/tmp/output.txt", "w")
 tmpfile:write(serialization.serialize(recipeTransposer, true))
